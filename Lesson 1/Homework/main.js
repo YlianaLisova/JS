d75fd5c11efb944fa35102cd2c1cc76208d8ce81
YlianaLisova <lisova229@gmail.com>
2022-02-01 02:08:03 +0200

    lesson 1

---
+++ Lesson 1/Homework/main.js
@@ -0,0 +1,131 @@
+// 1.Створити змінні.
+// let variable1 = 'hello';
+// let variable2 = 'owu';
+// let variable3 = 'com';
+// let variable4 = 'ua';
+// let variable5 = 1;
+// let variable6 = 10;
+// let variable7 = -999;
+// let variable8 = 123;
+// let variable9 = 3.14;
+// let variable10 = 2.7;
+// let variable11 = 16;
+// let variable12 = true;
+// let variable13 = false;
+
+//     2. Вивести кожну змінну
+//                    console.log
+// console.log(variable1);
+// console.log(variable2);
+// console.log(variable3);
+// console.log(variable4);
+// console.log(variable5);
+// console.log(variable6);
+// console.log(variable7);
+// console.log(variable8);
+// console.log(variable9);
+// console.log(variable10);
+// console.log(variable11);
+// console.log(variable12);
+// console.log(variable13);
+// //                        alert
+// alert(variable1)
+// alert(variable2)
+// alert(variable3)
+// alert(variable4)
+// alert(variable5)
+// alert(variable6)
+// alert(variable7)
+// alert(variable8)
+// alert(variable9)
+// alert(variable10)
+// alert(variable11)
+// alert(variable12)
+// alert(variable13)
+//                      document.write
+// document.write(variable1)
+// document.write(variable2)
+// document.write(variable3)
+// document.write(variable4)
+// document.write(variable5)
+// document.write(variable6)
+// document.write(variable7)
+// document.write(variable8)
+// document.write(variable9)
+// document.write(variable10)
+// document.write(variable11)
+// document.write(variable12)
+// document.write(variable13)
+
+// 3. Переприсвоїти кожній змінній з завдання значення на довільне.
+// variable1 = 'Hi'
+// variable2 = 'my';
+// variable3 = 'dear';
+// variable4 = 'student';
+// variable5 = 100500;
+// variable6 = 200600;
+// variable7 = true;
+// variable8 = 1223;
+// variable9 = 22.4;
+// variable10 = false;
+// variable11 = 2345678;
+// variable12 = -5;
+// variable13 = 17.11;
+
+// 4. Вивести кожну змінну
+//                    console.log
+// console.log(variable1);
+// console.log(variable2);
+// console.log(variable3);
+// console.log(variable4);
+// console.log(variable5);
+// console.log(variable6);
+// console.log(variable7);
+// console.log(variable8);
+// console.log(variable9);
+// console.log(variable10);
+// console.log(variable11);
+// console.log(variable12);
+// console.log(variable13);
+//                       alert
+// // alert(variable1)
+// // alert(variable2)
+// // alert(variable3)
+// // alert(variable4)
+// // alert(variable5)
+// // alert(variable6)
+// // alert(variable7)
+// // alert(variable8)
+// // alert(variable9)
+// // alert(variable10)
+// // alert(variable11)
+// // alert(variable12)
+// // alert(variable13)
+//                    document.write
+// // document.write(variable1)
+// // document.write(variable2)
+// // document.write(variable3)
+// // document.write(variable4)
+// // document.write(variable5)
+// // document.write(variable6)
+// // document.write(variable7)
+// // document.write(variable8)
+// // document.write(variable9)
+// // document.write(variable10)
+// // document.write(variable11)
+// // document.write(variable12)
+// // document.write(variable13)
+
+// 5. Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person.
+// let firstName = 'Уляна';
+// let middleName = 'Ігорівна';
+// let lastName = 'Лісова'
+//
+// let person = [firstName, lastName, middleName]
+
+// 6. За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.  Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// let firstName = 'Уляна';
+// let middleName = 'Ігорівна';
+// let age = 17;
+//
+// prompt('Вітаю' + ' ' + firstName +' ' + middleName + '.' + ' ' + 'Тобі' + ' ' + age + ' ' + 'років.')
