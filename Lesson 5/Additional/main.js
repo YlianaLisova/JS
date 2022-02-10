ea8b503e326adcd8faa2162b7a44339b6a40dbeb
YlianaLisova <lisova229@gmail.com>
2022-02-11 01:18:23 +0200

    lesson 5

---
+++ Lesson 5/Additional/main.js
@@ -0,0 +1,14 @@
+// 1. Дано натуральне число n. Выведите все числа от 1 до n.
+// let n = 21
+// function natureNum(counter) {
+//     console.log(counter);
+//     counter++;
+//     if (counter >= n) {
+//         return;
+//     } else {
+//         natureNum(counter);
+//     }
+// }
+// natureNum(1);
+
+// 2. Даны два целых числа A и В . Выведите все числа от A до B включаючи, в порядку зростання, якщо A < B, или в порядку убывания в іншому випадку.
