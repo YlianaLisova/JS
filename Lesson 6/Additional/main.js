fb4964e97feb7e2e63954a69632a8131fd0ab16c
YlianaLisova <lisova229@gmail.com>
2022-02-14 16:29:41 +0200

    lesson 6

--- Lesson 6/Additional/main.js
+++ Lesson 6/Additional/main.js
@@ -16,3 +16,137 @@
 // someeMAIL@gmail.com
 // someeMAIL@i.ua
 // some.email@gmail.com
+// let validator = (str) => {
+//     let a = '@';
+//     let b = '.';
+//     let lower = str.toLowerCase();
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === a && str[i + 1] === b) {
+//             console.log('Point must be two symbols after @')
+//         } else if (str[i] === a && str[i + 2] === b) {
+//             console.log('Point must be two symbols after @')
+//             return
+//         }
+//     }
+//     if (str.indexOf('@') === -1) {
+//         console.log('This email must contain @')
+//     }
+//     if (str.startsWith('@')) {
+//         console.log('Email can`t start with @')
+//     }
+//     if (str.indexOf('.') === -1) {
+// console.log('This email must contain point');
+//     } else {
+//         console.log(`${lower}`)
+//     }
+//     return lower;
+// }
+// validator('someemail@gmail.com');
+// validator('someeMAIL@gmail.com');
+// validator('someeMAIL@i.ua');
+// validator('some.email@gmail.com')
+
+// 3. є масив
+// let coursesArray = [
+//              {
+//                  title: 'JavaScript Complex',
+//                  monthDuration: 5,
+//                  hourDuration: 909,
+//                  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//              },
+//              {
+//                  title: 'Java Complex',
+//                  monthDuration: 6,
+//                  hourDuration: 909,
+//                  modules: ['html',
+//                      'css',
+//                      'js',
+//                      'mysql',
+//                      'mongodb',
+//                      'angular',
+//                      'aws',
+//                      'docker',
+//                      'git',
+//                      'java core',
+//                      'java advanced']
+//              },
+//              {
+//                  title: 'Python Complex',
+//                  monthDuration: 6,
+//                  hourDuration: 909,
+//                  modules: ['html',
+//                      'css',
+//                      'js',
+//                      'mysql',
+//                      'mongodb',
+//                      'angular',
+//                      'aws',
+//                      'docker',
+//                      'python core',
+//                      'python advanced']
+//              },
+//              {
+//                  title: 'QA Complex',
+//                  monthDuration: 4,
+//                  hourDuration: 909,
+//                  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//              },
+//              {
+//                  title: 'FullStack',
+//                  monthDuration: 7,
+//                  hourDuration: 909,
+//                  modules: ['html',
+//                      'css',
+//                      'js',
+//                      'mysql',
+//                      'mongodb',
+//                      'react',
+//                      'angular',
+//                      'aws',
+//                      'docker',
+//                      'git',
+//                      'node.js',
+//                      'python',
+//                      'java']
+//              },
+//              {
+//                  title: 'Frontend',
+//                  monthDuration: 4,
+//                  hourDuration: 909,
+//                  modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//              }
+//          ];
+// // відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+// coursesArray.sort((a,b) => b.modules.length-a.modules.length);
+// console.log(coursesArray);
+
+// 4.Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+// let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+// let symb = 'o';
+// let str = "Астрономия это наука о небесных объектах";
+// let count = (str, stringSearch) => {
+//     let counter = [];
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === stringSearch) {
+//             counter.push(str[i])
+//         }
+//     }
+//     return counter.length;
+// }
+// console.log(count(str, 'о'));
+
+// 5. Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
+//     let str = "Сила тяжести приложена к центру масс тела";
+// document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+// let str = "Сила тяжести приложена к центру масс тела";
+// let cutString = (str, n) => {
+//     let array = [];
+//     let string = str.split(' ');
+//     for (let i = 0; i < n; i++) {
+//         array.push(string[i]);
+//     }
+//     return array.join(' ')
+// }
+// document.writeln(cutString(str,5))
+
