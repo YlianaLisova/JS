86f58b29dcb493998f81df4a3686f73a82c5d51f
YlianaLisova <lisova229@gmail.com>
2022-02-18 18:52:42 +0200

    lesson 7

---
+++ Lesson 7/Additional/main.js
@@ -0,0 +1,18 @@
+// 1.
+// class User {
+//     constructor(id, personName, username, email, street, suite, city, zipcode, lat, lng, phone, website, companyName, catchPhrase, bs) {
+//         this.id = id;
+//         this.personName = personName;
+//         this.username = username;
+//         this.email = email;
+//         this.address = {street: street, suite: suite, city: city, zipcode: zipcode, geo: {lat: lat, lng: lng}};
+//         this.phone = phone;
+//         this.website = website;
+//         this.company = {companyName: companyName, catchPhrase: catchPhrase, bs: bs}
+//     }
+// }
+//
+// let user = new User(1, 'Leanne Graham', 'Bret', 'Sincere@april.biz', 'Kulas Light', 'Kulas Light', 'Gwenborough', '92998-3874', '-37.3159', '81.1496', '1-770-736-8031 x56442', 'hildegard.org', 'Romaguera-Crona', 'Multi-layered client-server neural-net', 'harness real-time e-markets')
+// console.log(user)
+
+//2.
