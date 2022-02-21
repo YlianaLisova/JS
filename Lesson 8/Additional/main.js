c20767f85f4afbad3ec9e44f3567c795fb68e7c5
YlianaLisova <lisova229@gmail.com>
2022-02-21 18:54:51 +0200

    lesson 8 additional

--- Lesson 8/Additional/main.js
+++ Lesson 8/Additional/main.js
@@ -1,11 +1,13 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
-// let array = [];
-// function scraper (startElement) {
-//     let children = startElement.children;
-//     for (const child of children) {
-//         scraper(child);
-//         array.push(child)
-//     }
-// }
-// scraper(document.body);
-// console.log(array);
+let array = [];
+function scraper (startElement) {
+    let children = startElement.children;
+    for (const child of children) {
+        for (const childElement of child.classList) {
+            array.push(childElement)
+        }
+        scraper(child);
+    }
+}
+scraper(document.body);
+console.log(array);
