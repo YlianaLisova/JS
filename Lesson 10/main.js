c98384b407f765403f56e016d992a14550440ce9
YlianaLisova <lisova229@gmail.com>
2022-02-24 23:57:51 +0200

    lesson 10

---
+++ Lesson 10/main.js
@@ -0,0 +1,33 @@
+// 1. - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
+// let input = document.getElementById('text');
+// let button = document.getElementById('button');
+// button.onclick = function () {
+//     input.style.display = 'none'
+// }
+
+
+// 2. - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// let button = document.getElementById('button');
+// button.onclick = function () {
+//     button.style.display = 'none'
+// }
+//
+// 3. - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію
+// з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+let input = document.getElementById('age');
+let button = document.getElementById('button2');
+button.onclick = function (e) {
+    if (input >= 18) {
+        console.log('Ти повнолітній!')
+    } else if (input < 18) {
+        console.log('Ти ще маленький :)')
+    }
+    e.preventDefault();
+}
+
+// - Создайте меню, которое раскрывается/сворачивается при клике
+//
+//
+// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+//     Вывести список комментариев в документ, каждый в своем блоке.
+//     Добавьте каждому комментарию по кнопке для сворачивания его body.
