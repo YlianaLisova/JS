f8bf6d1b34b5fee9b8bfec6c93eed60aeb781b98
YlianaLisova <lisova229@gmail.com>
2022-03-10 00:34:45 +0200

    lesson 11

---
+++ Lesson 11/Homework/main.js
@@ -0,0 +1,12 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+// let form1 = document.forms.form1;
+// form1.onsubmit = function (e) {
+//     e.preventDefault();
+//     localStorage.setItem('inputName', JSON.stringify({
+//         inputName: e.target.username.value,
+//         inputAge: e.target.age.value
+//     }))
+// }
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
