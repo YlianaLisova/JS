4ac087c95577aa175046483676c8a0c4c9799366
YlianaLisova <lisova229@gmail.com>
2022-03-26 04:12:41 +0200

    mini-project

--- mini-project/post-details.js
+++ mini-project/post-details.js
@@ -1,4 +1,21 @@
-let postWrapper = document.getElementsByClassName('post')[0];
 let newUrl = new URL(location.href);
 let post = JSON.parse(newUrl.searchParams.get('data'));
 console.log(post);
+let postWrapper = document.getElementsByClassName('post')[0];
+postWrapper.innerHTML = `
+<h4>UserId: ${post.userId}</h4>
+<h4>Id: ${post.id}</h4>
+<h4>Title: ${post.title}</h4>
+<h4>Body: ${post.body}</h4>
+`
+let commentsWrapper = document.getElementsByClassName('comments')[0];
+fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+    .then(value => value.json())
+    .then(value => {
+        for (const comment of value) {
+            let li = document.createElement('li');
+            li.classList.add('li');
+            li.innerText = comment.body;
+            commentsWrapper.appendChild(li);
+        }
+    })
